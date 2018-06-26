697911afa5032535ea81a750f262aa916b391665
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-06-26 12:08:51 -0500

    Degugger in chrome and command line

--- notes.js
+++ notes.js
@@ -50,7 +50,7 @@ let removeNote = (title) => {
 };
 
 let logNote = (note) => {
-    console.log('---');
+    console.log('--');
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
 };
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Nickolas"
+};
+
+person.age = 25;
+
+debugger;
+
+person.name = "Mike";
+
+console.log(person);
