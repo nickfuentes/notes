da3d345b79facb2e69d6e0ac909817144dea126e
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-06-26 12:43:51 -0500

    Refactored the add, remove, and read functionality

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions,
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 let command = argv._[0];
-console.log('Command', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
     let note = notes.addNote(argv.title, argv.body);
